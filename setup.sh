44516dab480b9a0e09742015082d9c4929455bd7
Stephen Shank <sshank314@gmail.com>
2021-06-23 10:13:51 -0400

    Fix raxml-ng version in setup script.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 printf "\nSETTING UP ENV: rascl\n"
 
-conda create -n rascl biopython=1.70 hyphy=2.5.31 python-bioext=0.19.7 raxml-ng=1.02 snakemake=5.3 -y 
+conda create -n rascl biopython=1.70 hyphy=2.5.31 python-bioext=0.19.7 raxml-ng=1.0.2 snakemake=5.3 -y 
 
 printf "\nSUCCESSFULL INSTALL\n"
 
